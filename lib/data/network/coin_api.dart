c58d8f2a8bba90e4d4c76c1292ab9a47bf0ac00c
Kenneth Terrago <kterrago091893@gmail.com>
2022-05-03 13:38:10 +0800

    feat: add coin api

---
+++ lib/data/network/coin_api.dart
@@ -0,0 +1,13 @@
+import 'package:http/http.dart' as http;
+
+import '../../constants/strings.dart';
+
+class CoinApi {
+  Future<http.Response> loadCoins() async {
+    final uriParsed = Uri.parse('${Strings.baseApiUrl}/coins');
+
+    final response = await http.get(uriParsed);
+
+    return response;
+  }
+}
