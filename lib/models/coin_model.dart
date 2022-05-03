201c163ab02b1e97be06e3c6cab61034aa69af6c
Kenneth Terrago <kterrago091893@gmail.com>
2022-05-03 16:43:55 +0800

    refactor: coin model

--- lib/models/coin_model.dart
+++ lib/models/coin_model.dart
@@ -16,27 +16,51 @@ class Coin {
     this.name,
     this.symbol,
     this.rank,
-    this.isNew,
-    this.isActive,
-    this.type,
+    this.priceUsd,
+    this.priceBtc,
+    this.volume24HUsd,
+    this.marketCapUsd,
+    this.circulatingSupply,
+    this.totalSupply,
+    this.maxSupply,
+    this.percentChange1H,
+    this.percentChange24H,
+    this.percentChange7D,
+    this.lastUpdated,
   });
 
   String? id;
   String? name;
   String? symbol;
-  int? rank;
-  bool? isNew;
-  bool? isActive;
-  String? type;
+  String? rank;
+  String? priceUsd;
+  String? priceBtc;
+  String? volume24HUsd;
+  String? marketCapUsd;
+  String? circulatingSupply;
+  String? totalSupply;
+  String? maxSupply;
+  String? percentChange1H;
+  String? percentChange24H;
+  String? percentChange7D;
+  String? lastUpdated;
 
   factory Coin.fromJson(Map<String, dynamic> json) => Coin(
         id: json["id"],
         name: json["name"],
         symbol: json["symbol"],
         rank: json["rank"],
-        isNew: json["is_new"],
-        isActive: json["is_active"],
-        type: json["type"],
+        priceUsd: json["price_usd"],
+        priceBtc: json["price_btc"],
+        volume24HUsd: json["volume_24h_usd"],
+        marketCapUsd: json["market_cap_usd"],
+        circulatingSupply: json["circulating_supply"],
+        totalSupply: json["total_supply"],
+        maxSupply: json["max_supply"],
+        percentChange1H: json["percent_change_1h"],
+        percentChange24H: json["percent_change_24h"],
+        percentChange7D: json["percent_change_7d"],
+        lastUpdated: json["last_updated"],
       );
 
   Map<String, dynamic> toJson() => {
@@ -44,8 +68,16 @@ class Coin {
         "name": name,
         "symbol": symbol,
         "rank": rank,
-        "is_new": isNew,
-        "is_active": isActive,
-        "type": type,
+        "price_usd": priceUsd,
+        "price_btc": priceBtc,
+        "volume_24h_usd": volume24HUsd,
+        "market_cap_usd": marketCapUsd,
+        "circulating_supply": circulatingSupply,
+        "total_supply": totalSupply,
+        "max_supply": maxSupply,
+        "percent_change_1h": percentChange1H,
+        "percent_change_24h": percentChange24H,
+        "percent_change_7d": percentChange7D,
+        "last_updated": lastUpdated,
       };
 }
